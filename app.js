5b566cd5b06f267d557f497dbdb4ce0d87552459
Mazharrais <mazharmajo72@gmail.com>
2024-07-31 15:09:37 +0500

    add code

---
+++ app.js
@@ -0,0 +1,12 @@
+
+var nameErorr = document.getElementById('name-error');
+var phoneErorr = document.getElementById('phone-error');
+var emailErorr = document.getElementById('email-error');
+var messageErorr = document.getElementById('message-error');
+var submitErorr = document.getElementById('submit-error');
+
+
+function validationName(){
+    var name = document.getElementById('contact-name').ariaValueMax;
+    console.log(name);
+}
